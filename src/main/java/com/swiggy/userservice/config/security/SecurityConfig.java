2406e52805af3294edcd7796dcbdf232a783efbe
Sarthak Rohatgi <sarthak.rohatgi_ftc@external.swiggy.in>
2024-03-10 20:43:18 +0530

    authorize fulfillment service request

--- src/main/java/com/swiggy/userservice/config/security/SecurityConfig.java
+++ src/main/java/com/swiggy/userservice/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.swiggy.userservice.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -22,10 +24,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        IpAddressMatcher hasIpAddress = new IpAddressMatcher("127.0.0.1");
+
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) -> {
                     authorize.requestMatchers("/h2-console/**").permitAll()
                             .requestMatchers(HttpMethod.POST, "/user-management/users").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/user-management/users").access((authentication, context) -> new AuthorizationDecision(hasIpAddress.matches(context.getRequest())))
                             .requestMatchers(HttpMethod.GET, "/user-management/users").hasAuthority("ADMIN")
                             .anyRequest().authenticated();
                 })
